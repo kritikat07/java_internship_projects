55ba821c9f47b8b0c261d53e16ef6e57d96c71c1
Kritika Tiwari <tiwarikritika178@gmail.com>
2026-07-23 23:08:24 +0530

    Refactor Arraylist.java for better readability

--- java_programs/src/Collection_framework/Arraylist.java
+++ java_programs/src/Collection_framework/Arraylist.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class Arraylist {
     public static void main(String[] args) {
         ArrayList<String> fruits = new ArrayList<>();
-
         // Adding elements
         fruits.add("Apple");
         fruits.add("Banana");
@@ -14,12 +13,10 @@ public class Arraylist {
 
         // Accessing elements
         System.out.println("First fruit: " + fruits.get(0));
-
         // Iterating elements
         for(String fruit : fruits) {
             System.out.println(fruit);
         }
-
         // Removing elements
         fruits.remove("Banana");
         System.out.println("After removal: " + fruits);
